8b6df3b9a478c6f31a6277b4c24062735b09e2b3
Glad-BR <gradbeerro@proton.me>
2026-06-22 07:24:30 -0300

    ?

--- setup.py
+++ setup.py
@@ -1,20 +1,48 @@
-from setuptools import setup, Extension
-import subprocess
-import os
-
+import json, zipfile, tarfile, shutil
+from urllib.request import urlopen, urlretrieve
 from pathlib import Path
 
-# Build C++ app before Python package
-def build_cpp():
-    cpp_dir = (Path(__file__).parent  / "cpp" / "MareTF")
-    # Run CMake, make, etc.
-    subprocess.run(["cmake", ".", "-B", "build"], cwd=cpp_dir, check=True)
-    subprocess.run(["cmake", "--build", "build"], cwd=cpp_dir, check=True)
 
-setup(
-    name="mywrapper",
-    version="0.1.0",
-    package_dir={"": "src"},
-    packages=["mywrapper"],
-    # ... other setup args
-)
+#idk what im doing
+
+def fetch_maretf(dest='scr/PyVTFLib/bin'):
+    tmp = Path('tmp')
+    tmp.mkdir(exist_ok=True)
+    
+    releases = {
+        'MareTF-Linux-x86_64.tar.zst':(
+            'linux64',
+            'maretf',
+            tarfile.open,
+            {'filter':'data'}
+        ),
+        'MareTF-Windows-x86_64.zip':(
+            'win64',
+            'maretf.exe',
+            zipfile.ZipFile,
+            {}
+        )
+    }
+    
+    for asset in json.loads(urlopen("https://api.github.com/repos/craftablescience/MareTF/releases/latest").read())["assets"]:
+        if asset['name'] in releases:
+            print(f"Downloading {asset['name']}...")
+            urlretrieve(asset["browser_download_url"], tmp / asset['name'])
+    
+    for fname, (subdir, exe, opener, kwargs) in releases.items():
+        extract_dir = tmp / subdir
+        extract_dir.mkdir(exist_ok=True)
+        
+        with opener(tmp / fname, "r") as archive:
+            archive.extractall(path=extract_dir, **kwargs)
+        
+        target = Path(dest) / subdir / exe
+        target.parent.mkdir(parents=True, exist_ok=True)
+        target.unlink(missing_ok=True)
+        shutil.move(next(extract_dir.rglob(exe)), target)
+        shutil.rmtree(extract_dir)
+    
+    shutil.rmtree(tmp)
+
+if __name__ == "__main__":
+    fetch_maretf()
